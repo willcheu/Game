8aaedc28cf5a9a91eefbd4938070b84feb90306c
willcheu <willcheu47@gmail.com>
2024-02-12 23:32:07 +1100

    added rects

--- main.cpp
+++ main.cpp
@@ -11,45 +11,42 @@ int main()
 {
     // Create the main window
     sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-
     // Load a sprite to display
     sf::Texture texture;
-    sf::RectangleShape rect;
-    rect.setSize(sf::Vector2f(100,50));
-    rect.setOutlineColor(sf::Color::Green);
-    rect.setOutlineThickness(5);
-    rect.setPosition(0,20);
-    std::vector<sf::RectangleShape> rectangles;
-    sf::RectangleShape left;
-    left.setSize(sf::Vector2f(25,100));
+
+    sf::RectangleShape left(sf::Vector2f(25,100));
+
     left.setFillColor(sf::Color::White);
-    left.setPosition(25, 100);
-    sf::RectangleShape;
+    left.setPosition(0, 250);
+    sf::RectangleShape right(sf::Vector2f(25,100));
+    right.setPosition(775,250);
+    left.setFillColor(sf::Color::White);
+
+
     float width = 0;
     bool start = false;
 
     // Start the game loop
     while (window.isOpen())
     {
-
         // Process events
         sf::Event event;
-
-
         while (window.pollEvent(event))
         {
-
-
             // Close window: exit
             if (event.type == sf::Event::Closed)
                 window.close();
         }
-
         // Clear screen
+
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+            right.move(0, 100);
+            std::cout << "m";
+        }
         window.clear();
         window.draw(left);
-
-
+        window.draw(right);
         // Update the window
         window.display();
 
